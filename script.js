138fd15aa17444a70e395abb6f8ec2711bcc31e2
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-04-20 18:39:10 -0400

    ArrayAverage

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ console.log("Functions ALAB")
     let result = [24 + 25 + 13 + 2 + 15];
     return result;
 }
+const array = [24, 25, 13, 2, 15];
+function avgOfArr(array){
+    if (array.length === 0) 
+    return 'empty array';
+return array.reduce((acc, c) => acc + c, 0) / array.length;
+}
  let answer = sumOfArr(24, 25, 13, 2, 15);
  console.log(answer);
+ 
+console.log(avgOfArr(array));
