970c93fef15335239a6cb5d44aa64b0950018450
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-04-20 17:34:18 -0400

    Part1changes

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Functions ALAB")
+
+// Part 1 Thinking Functionally
+ function sumOfArr(){
+    let result = [24 + 25 + 13 + 2 + 15];
+    return result;
+}
+ let answer = sumOfArr(24, 25, 13, 2, 15);
+ console.log(answer);
