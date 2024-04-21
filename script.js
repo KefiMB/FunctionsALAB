ac3681fc19390634b4682e59ecd32b1e8c1c5df1
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-04-20 20:51:43 -0400

    StringLongerThanNumber

--- script.js
+++ script.js
@@ -33,3 +33,18 @@ function longestStr(str){
 const longestString = longestStr(str);
 console.log(longestString);
 
+const strNum = ["Jasper", "is", 4];
+function StrLongerThanNum(strNum){
+    if (strNum.length === 0) return "";
+    let stringLongerThanNum = strNum[0];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].length > stringLongerThanNum.length) {
+            longest = strNum[i]; 
+        }
+    }
+
+    return stringLongerThanNum; 
+}
+const longStr = StrLongerThanNum(strNum);
+console.log(longStr);
